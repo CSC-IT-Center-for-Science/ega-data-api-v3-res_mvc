aaac6fb97d301cd28515ffd7f2f332e4ef4a5eb2
asenf <asenf@asenf-virtual-machine>
2017-05-25 12:45:15 +0100

    Update Docker Files

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+git clone https://github.com/elixir-europe/ega-data-api-v3-cipher.git
+mvn -f /ega-data-api-v3-cipher/pom.xml install
 git clone https://github.com/elixir-europe/ega-data-api-v3-res_mvc.git
 mvn -f /ega-data-api-v3-res_mvc/pom.xml install
 mv /ega-data-api-v3-res_mvc/target/ReEncryptionMVC-0.0.1-SNAPSHOT.jar /EGA_build
